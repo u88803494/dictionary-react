5b6f4460f3492b6c147fa0cc003ff1b320c9857e
Hugh Lee <u88803494@gmail.com>
2021-08-18 20:35:00 +0800

    Complete the basic structure of the word

--- src/components/Dictionary/Dictionary.jsx
+++ src/components/Dictionary/Dictionary.jsx
@@ -1,21 +1,41 @@
 // region 1. Platform Libraries
 import axios from 'axios';
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 // end-region
 
 const Dictionary = ({ word }) => {
+  const [heteronyms, setHeteronyms] = useState([]);
   useEffect(() => {
-    axios.get(` https://www.moedict.tw/raw/${word}`)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => console.log(err));
+    if (word) {
+      axios.get(` https://www.moedict.tw/raw/${word}`)
+        .then((res) => setHeteronyms(res.data.heteronyms))
+        .catch((err) => console.log(err));
+    }
+    if (!word) {
+      setHeteronyms([]);
+    }
   }, [word]);
   return (
     <div className="flex justify-center">
-      <div className="h-96 w-2/3 flex flex-col">
-        <div>字義</div>
+      <div className="h-96 w-2/3 mt-8 flex flex-col">
+        <div className="heteronyms">
+          {heteronyms.map((heteronym) => (
+            <div className="flex">
+              <div className="word bg-white text-black text-7xl">
+                {word}
+              </div>
+              <div className="pronunciation-container">
+                <div className="bopomofo text-4xl">
+                  {`注：${heteronym.bopomofo}`}
+                </div>
+                <div className="romanization text-4xl">
+                  {`拼：${heteronym.bopomofo2}`}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
         <div>解釋</div>
       </div>
     </div>
