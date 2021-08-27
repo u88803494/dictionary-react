54b5c93b0d07eaa8000aa67b5ceb05d0fb004e66
Hugh Lee <u88803494@gmail.com>
2021-08-27 17:31:05 +0800

    Remove no longer needed class name

--- src/components/Dictionary/DictionaryRender.jsx
+++ src/components/Dictionary/DictionaryRender.jsx
@@ -5,30 +5,30 @@ import React from 'react';
 
 const DictionaryRender = ({ details }) => (
   <div className="flex justify-center">
-    <div className="words h-96 w-2/3 mt-8 flex flex-col">
+    <div className="h-96 w-2/3 mt-8 flex flex-col">
       {details.map((heteronym) => (
-        <div className="word">
-          <div className="heteronyms flex">
+        <>
+          <div className="flex">
             {heteronym.pronunciations.map(({ pronunciation1, pronunciation2, word }) => (
-              <div className="heteronym flex mr-8">
-                <div className="mr-2 bg-white text-black text-7xl">
+              <div className="flex mr-8">
+                <div className="w-20 mr-2 bg-white text-black text-center text-7xl">
                   {word}
                 </div>
-                <div className="pronunciations-container">
-                  <div className="bopomofo text-4xl">
+                <div>
+                  <div className="text-4xl">
                     {pronunciation1}
                   </div>
-                  <div className="romanization pl-2 text-4xl">
+                  <div className="pl-2 text-4xl">
                     {pronunciation2}
                   </div>
                 </div>
               </div>
             ))}
           </div>
-          <div className="definition">
+          <div className="definitions">
             解釋
           </div>
-        </div>
+        </>
       ))}
     </div>
   </div>
